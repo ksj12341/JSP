30ac5f66c81964db35ec0ea389f1819704b65625
ksj12341 <127382032+ksj12341@users.noreply.github.com>
2023-03-30 11:51:22 +0900

    Add files via upload

--- Chapter07/src/sec02/ex02/MemberDAO.java
+++ Chapter07/src/sec02/ex02/MemberDAO.java
@@ -69,5 +69,48 @@ public class MemberDAO {
 		}
 		return list;
 	}
+	public void addMember(MemberVO memberVO)
+	{
+		try {
+			con = dataFactory.getConnection();
+			String id = memberVO.getId();
+			String pwd = memberVO.getPwd();
+			String name = memberVO.getName();
+			String email = memberVO.getEmail();
+			
+			String query = "insert into t_member";
+			query += " (id,pwd,name,email)";
+			query += " values(?,?,?,?)";
+			System.out.println("prepareStatement: " + query);
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.setString(2, pwd);
+			pstmt.setString(3, name);
+			pstmt.setString(4, email);
+			pstmt.executeUpdate();
+			pstmt.close();
+	}	catch (Exception e)
+		{
+			e.printStackTrace();
+					
+		}
+	}
+	public void delMember(String id) {
+		try {
+			con = dataFactory.getConnection();
+			
+			String query = "delete from t_member" +" where id=?";
+			System.out.println("prepareStatement: " + query);
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, id);
+			pstmt.executeUpdate();
+			pstmt.close();
+			
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+					
+		}
+	}
 	
 }
--- Chapter07/src/sec02/ex03/MemberServlet.java
+++ Chapter07/src/sec02/ex03/MemberServlet.java
@@ -1,4 +1,4 @@
-package sec02.ex02;
+package sec02.ex03;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberServlet
  */
-@WebServlet("/member4")
+@WebServlet("/member5")
 public class MemberServlet extends HttpServlet {
 
 
@@ -48,27 +48,33 @@ public class MemberServlet extends HttpServlet {
 		vo.setName(_name);
 		vo.setEmail(_email);
 		dao.addMember(vo);
-		}
-		List<MemberVO> list = dao.listMembers();
+		}else if (command != null && command.equals("delMember"))
+	      {
+	         String id =request.getParameter("id");
+	         dao.delMember(id);
+	      }
+
+		List<sec02.ex03.MemberVO> list = dao.listMembers();
 		out.print("<html><body>");
 		out.print("<table border=1><tr align='center' bgcolor='lightgreen'>");
 		out.print("<td>아이디</td><td>비밀번호</td><td>이름</td><td>이메일</td><td>가입일</td><tr>");
 		
-		for (int i = 0; i < list.size(); i++) {
-			MemberVO memberVO = list.get(i);
-			String id = memberVO.getId();
-			String pwd = memberVO.getPwd();
-			String name = memberVO.getName();
-			String email = memberVO.getEmail();
-			Date joinDate = memberVO.getJoinDate();
-			out.print("<tr><td>" + id + "</td><td>" + pwd +
-					"</td><td>" + name + "</td><td>" + email + "</td><td>" +
-					joinDate +"</td><td>" );
+		  for (int i = 0; i < list.size(); i++) {
+		         sec02.ex03.MemberVO memberVO = list.get(i);
+		         String id = memberVO.getId();
+		         String pwd = memberVO.getPwd();
+		         String name = memberVO.getName();
+		         String email = memberVO.getEmail();
+		         Date joinDate = memberVO.getJoinDate();
+		         
+		         out.print("<tr><td>" + id + "</td><td>" + pwd + "</td><td>" + name + "</td><td>" + email + "</td><td>" + joinDate + "</td><td>"
+		               +"<a href='/Chapter07/Member5?command=delMember&id="+id+"'>삭제 </a></td></tr>");
+		      }
+		      out.print("</table></body></html>");
+		      
+		      out.print("<a herf='/Chapter07/Memberform.html'>새 회원 등록하기</a>");
+		   }
+
+
 		}
-			out.print("</table></body></html>");
-			out.print("<a href='/Chapter07/memberForm.html'>새 회원 등록하기 </a>");
-			
-		}
-	}
-	
 
